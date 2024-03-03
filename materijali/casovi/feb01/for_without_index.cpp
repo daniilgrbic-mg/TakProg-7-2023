7a494e509f73082ef61df4c917d6c3d3c0b48f16
daniilgrbic <daniilgrbic@gmail.com>
2024-03-03 12:39:20 +0100

    Ubačeni zaostali stariji kodovi

---
+++ materijali/casovi/feb01/for_without_index.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // int n = 10;
+    // for(int i = 0; i < n; i++) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    // for(double i = 5.0; i < 10.0; i = i + 0.5) {
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    int n;
+    cin >> n;
+    vector <int> vek(n);
+    for(int i = 0; i < n; i++) {
+        cin >> vek[i];
+    }
+
+    // for(int i = 0; i < n; i++) {
+    //     cout << pow(vek[i], 2) << endl;
+    // }
+
+    // for(int el : vek) {
+    //     cout << pow(el, 2) << endl;
+    // }
+
+    return 0;
+}
---
+++ materijali/casovi/feb01/getline_vs_cin.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string ulaz;
+    // cin >> ulaz;
+    getline(cin, ulaz);
+    cout << "String je '" << ulaz << "'" << endl;
+    return 0;
+}
