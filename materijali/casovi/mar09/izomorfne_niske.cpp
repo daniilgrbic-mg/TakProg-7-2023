0eea92ab47337ff9f7ab7f1d9d622dc0cf2be168
Daniil Grbić <daniilgrbic@protonmail.com>
2024-03-09 12:50:41 +0100

    Popravljena greška u zadatku 'Izomorfne niske'

--- materijali/casovi/mar09/izomorfne_niske.cpp
+++ materijali/casovi/mar09/izomorfne_niske.cpp
@@ -2,28 +2,31 @@
 #include <string>
 #include <map>
 
-#include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
     string a, b;
     cin >> a >> b;
 
-    map <char, char> m;
-    map <char, char> minv;
+    if(a.size() != b.size()) {
+        cout << "ne" << endl;
+        return 0;
+    }
+
+    map <char, char> m;    // slike iz a u b
+    map <char, char> minv; // slike iz b u a (inverz)
 
     for(int i = 0; i < a.size(); i++) {
         char ai = a[i];
         char bi = b[i];
 
-        // da li se jedno slovo slika u vise
+        // da li se jedno slovo slika u vise (to ne sme)
         if(m.count(ai) && m[ai] != bi) {
             cout << "ne" << endl;
             return 0;
         }
 
-        // da li se vise slova slikaju u jedno
+        // da li se vise slova slikaju u jedno (to ne sme)
         if(minv.count(bi) && minv[bi] != ai) {
             cout << "ne" << endl;
             return 0;
